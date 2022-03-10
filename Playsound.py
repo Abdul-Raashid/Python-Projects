366664a7eed7d25b534691dfd1b7b19c48722ee9
Abdul-Raashid <101323777+Abdul-Raashid@users.noreply.github.com>
2022-03-10 14:57:47 +0530

    Add files via upload

---
+++ Playsound.py
@@ -0,0 +1,11 @@
+from playsound import playsound
+
+playsound('D:\\SEM-4\\program files\\Chapter-1\\Everyday Normal.mp3')
+playsound('D:\\SEM-4\\program files\\Chapter-1\\whosays.mp3')
+playsound('D:\\SEM-4\\program files\\Chapter-1\\play.mp3')
+
+
+# Path of an Mp3 file in your pc D:\SEM-4\program files\Chapter-1
+#playsound is an module
+#pip install playsound==1.2.2  --This version only works other gives error
+
